b9e3eecd476f3d7a7aa026d3739458958b0d6a5a
rohitsahu <iamrohit2112@gmail.com>
2025-04-08 21:34:09 +0530

    Implemented user register, login and logout routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, Req, Res, ValidationPipe } from "@nestjs/common
 import { Response } from "express";
 import { logindto } from "src/dto/login.dto";
 import { AuthService } from "./auth.service";
+import { UserType } from "src/database/entities/enums";
 
 @Controller('auth')
 export class AuthController{
@@ -9,13 +10,90 @@ export class AuthController{
     constructor(private authService : AuthService){}
 
     @Post('user/login')
-    loginNormalUser(){
+    async loginNormalUser( @Body( new ValidationPipe() ) loginPayload : logindto, @Res() res : Response ){
+        try{
+            const {accessToken, refreshToken, userData} = await this.authService.loginUser(loginPayload);
 
+            res.cookie(
+                'accessToken',
+                accessToken,
+                {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite : 'strict',
+                    maxAge: 1 * 24 * 60 * 60 * 1000
+                }
+            )
+
+            res.cookie(
+                'refreshToken',
+                refreshToken,
+                {
+                    httpOnly: true,
+                    secure: true,
+                    sameSite : 'strict',
+                    maxAge: 7 * 24 * 60 * 60 * 1000,
+                }
+            )
+
+            res.status(201).json({
+                success : true,
+                message : "Login Successul",
+                accessToken,
+                refreshToken,
+                ...userData
+            })
+        }
+        catch(error){
+            return res.status(error.status || 500).json({
+                error : error.message
+            })
+        }
     }
 
     @Post('user/logout')
-    logout(){
+    async logout( @Req() req: Request, @Res() res: Response ){
 
+        try{
+            const user = req['user'];
+            if(!user || user.userType !== UserType.User){
+                return res.status(401).json({
+                    message : 'Unauthorized'
+                })
+            } 
+
+            const logOutUser = await this.authService.logoutUser(user.id)
+
+            if(logOutUser){
+                res.clearCookie(
+                    'accessToken',
+                    {
+                        httpOnly : true,
+                        secure : true,
+                        sameSite : 'strict'
+                    }
+                )
+
+                res.clearCookie(
+                    'refreshToken',
+                    {
+                        httpOnly : true,
+                        secure : true,
+                        sameSite : 'strict'
+                    }
+                )
+
+                return res.status(200).
+                json({
+                    success : true,
+                    message : "Logged out successfully"
+                })
+            }
+        } catch (error){
+            return res.status(500).json({
+                message : 'Something went wrong during logout',
+            error : error.error.message            })
+        }
     }
 
     @Post('doctor/login')
@@ -47,10 +125,12 @@ export class AuthController{
             )
 
             res.status(201).json({
+                success: true,
                 message: "Login Successful",
-                userDetails,
+                
                 accessToken,
-                refreshToken
+                refreshToken,
+                ...userDetails,
             })
             
             
@@ -67,9 +147,10 @@ export class AuthController{
 
             
             const user = req['user']
-            if(!user){
+            if(!user || user.userType !== UserType.Doctor){
                 return res.status(401).json({ message: 'Unauthorized' })
             }
+
 
             console.log("Auth Controller");
             
@@ -93,7 +174,9 @@ export class AuthController{
                     }
                 )
 
-                return res.status(200).json({ message: "Logged out successfully" })
+                return res.status(200).json({
+                    success : true,
+                    message: "Logged out successfully" })
             }
 
             
--- src/doctors/doctor.controller.ts
+++ src/doctors/doctor.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, InternalServerErrorException, Post, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, HttpException, HttpStatus, InternalServerErrorException, Post, ValidationPipe } from "@nestjs/common";
 import { registerdto } from "src/dto/register.dto";
 import { DoctorService } from "./doctor.service";
 import { registerDoctorParams } from "src/utils/types";
 import { UserType } from "src/database/entities/enums";
+import { error } from "console";
 
 @Controller('doctor')
 export class DoctorController{
@@ -12,13 +13,28 @@ export class DoctorController{
     @Post('register')
     async registerDoctor(@Body( new ValidationPipe()) doctor: registerdto){
 
-            const doctorData: registerDoctorParams = {
-                name: doctor.name,
-                email: doctor.email,
-                phone: doctor?.phone || "",
-                password: doctor.password,
-                userType: UserType.Doctor
-            }
-        return await this.doctorService.registerDoctor(doctorData);
+           try {
+             const doctorData: registerDoctorParams = {
+                 name: doctor.name,
+                 email: doctor.email,
+                 phone: doctor?.phone || "",
+                 password: doctor.password,
+                 userType: UserType.Doctor
+             }
+
+             const savedDoctor = await this.doctorService.registerDoctor(doctorData);
+
+             if(!savedDoctor){
+                throw new HttpException("Something went wrong while creating doctor.", HttpStatus.INTERNAL_SERVER_ERROR);
+             }
+
+             return {
+                "success": true,
+                "message": "User created successfully",
+                ...savedDoctor
+             }
+           } catch (error) {
+                throw new HttpException(error, HttpStatus.BAD_REQUEST);
+           }
     }
 }
